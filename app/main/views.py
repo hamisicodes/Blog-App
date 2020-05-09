f72b7ba9193710e0da5584f0fa8eec9082845abc
hamisicodes <newhamisi@gmail.com>
2020-05-09 17:38:58 +0300

    define the basic UI for the app

--- app/main/views.py
+++ app/main/views.py
@@ -3,5 +3,10 @@ from flask import render_template
 
 @main.route('/')
 def index():
-    title = 'Hello World'
+    title = 'welcome'
     return render_template('index.html', title=title)
+
+@main.route('/blogs')
+def blogs():
+    title = 'all blogs'
+    return render_template('blogs.html', title=title)
